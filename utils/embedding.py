d5a686c8daa0181b1f927148b74dbd9e12aab488
dieumerci7 <yarema.dekhtiar@gmail.com>
2023-12-08 13:37:55 +0200

    Add embedding.py, requirements.txt

---
+++ utils/embedding.py
@@ -0,0 +1,11 @@
+from angle_emb import AnglE, Prompts
+
+
+def embed(txt):
+    angle = AnglE.from_pretrained('WhereIsAI/UAE-Large-V1', pooling_strategy='cls')
+    angle.set_prompt(prompt=Prompts.C)
+    return angle.encode({'text': txt}, to_numpy=True)
+
+
+if __name__ == "__main__":
+    print(embed("Hello, World!!!"))
