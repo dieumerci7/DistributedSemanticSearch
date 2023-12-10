73e187d979be8ef6199a59a1be74ec09a997f7d3
ThorIgor <44617152+ThorIgor@users.noreply.github.com>
2023-12-11 00:25:15 +0200

    load balancer init

---
+++ server/client.py
@@ -0,0 +1,18 @@
+import requests
+
+def send_data():
+    url = 'http://localhost:80'
+    data = {'query': 'Cool query'}
+    try:
+        response = requests.get(url, params=data)
+
+        if response.status_code == 200:
+            print("Data sent successfully")
+            print(f"Response: {response.json()}")
+        else:
+            print("Failed to send data. Status code:", response.status_code)
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    send_data()
---
+++ server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import socket
+from sklearn.metrics.pairwise import cosine_similarity
+from embedding import embed
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def receive_data():
+    print(socket.gethostname())
+    try:
+        data = request.args.to_dict()  # Assumes the client sends JSON data
+        print("Received data:", data)
+        return {'abstract': 'Cool abstract', 'hostname': str(socket.gethostname()), 'status': 'success'}
+    except Exception as e:
+        print("Error processing request:", e)
+        return {'status': 'error'}
+
+if __name__ == '__main__':
+    app.run(debug = True)
